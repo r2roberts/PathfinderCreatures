474df6305ff564759fdcc2d418ec854db720d31a
Randy M. Roberts <rsqrdroberts@gmail.com>
2020-05-21 14:50:41 -0600

    o -- makinig progress on autogenerating html with python

---
+++ python/Racharak.py
@@ -0,0 +1,30 @@
+import creature
+from creature import Actions
+
+c = creature.Creature("Racharack", lvl=8)
+c.traits("UNIQUE", "CE", "SMALL", "CHARAU-KA", "DRAGON", "FIRE", "HUMANOID")
+c.perception("+16; darkvision, scent(imprecise) 30 feet")
+c.languages("Draconic, Mwangi")
+c.skills("Arcana +12", "Athletics +18", "Crafting +10",
+         "Intimidation +17", "Religion +14", "Stealth +15", "Survival +14")
+c.attrs("Str +6", "Dex +3", "Con +5", "Int +0", "Wis +4", "Cha +1")
+c.ac("27")
+c.saves("Fort +19", "Ref +13", "Will +16")
+c.hp("135")
+c.immunities("fire, paralyzed, sleep")
+c.interaction_ability("Tail Swipe", action=Actions.REACTION, trigger="A creature within reach of Racharak’s tail uses a move action or leaves a square during a move action it’s using.",
+                      effect="Racharak makes a tail Strike at the creature with a –2 penalty. If it hits, she disrupts the creature’s action.")
+c.speed("30 feet, climb 30 feet")
+c.melee("jaws + 20", action=Actions.ONE, damage="2d8+9 piercing plus 2d4 fire")
+c.melee("claw + 20 (agile)", action=Actions.ONE, damage="2d8+9 slashing")
+c.melee("tail + 20 (reach 10 feet)",
+        action=Actions.ONE, damage="2d8+9 slashing")
+c.ranged("thrown rock + 17 (deadly 1d6, thrown 20 feet)",
+         action=Actions.ONE, damage="2d6+9 bludgeoning")
+c.offensive_ability("Breath Weapon", action=Actions.TWO,
+                    desc="Racharak breathes flames that deals 9d6 fire damage to all creatures in a 30-foot cone(DC 26 basic Reflex save). She can’t use Breath Weapon again for 1d4 rounds.")
+c.offensive_ability("Draconic Frenzy", action=Actions.TWO,
+                    desc="Racharak makes two claw Strikes and one bite Strike in any order.")
+c.offensive_ability("Shrieking Frenzy", action=Actions.FREE,
+                    trigger="The charau-ka’s turn begins.", frequency="once per hour", effect="The charau-ka is quickened until the end of its turn, and can use the extra action to Stride or Strike. While in the frenzy, the charau-ka can’t speak and automatically critically fails Stealth checks, due to its loud wailing.")
+c.offensive_ability("Thrown Weapon Mastery", desc="Any weapon a charau-ka throws gains the deadly d6 weapon trait. Furthermore, when a charau-ka throws an improvised weapon, it does not take the –2 penalty for doing so, nor does it take a penalty for using a thrown improvised weapon with the nonlethal trait to make a lethal attack.")
---
+++ python/creature.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+
+class Actions(Enum):
+    FREE = 0
+    REACTION = -1
+    ONE = 1
+    TWO = 2
+    THREE = 3
+
+
+class Creature(object):
+    def __init__(self, name, lvl=0) -> None:
+        self._interaction_abilities = []
+        self._offensive_abilities = []
+        self._melees = []
+        self._ranged = []
+
+    def traits(self, *traits) -> None:
+        self._traits = list(traits)
+        print(self._traits)
+
+    def perception(self, perception: str):
+        self._perception = perception
+
+    def languages(self, languages: str):
+        self._languages = languages
+
+    def skills(self, *skills):
+        self._skills = skills
+
+    def attrs(self, *attrs):
+        self._attrs = attrs
+
+    def ac(self, ac: str):
+        self._ac = ac
+
+    def saves(self, *saves):
+        self._saves = saves
+
+    def hp(self, hp: str):
+        self._hp = hp
+
+    def immunities(self, immunities: str):
+        self._immunities = immunities
+
+    def interaction_ability(self, name: str, action: Actions = None, trigger: str = None, effect: str = None,
+                            frequency: str = None):
+        self._interaction_abilities.append({"name": name, "action": action, "trigger": trigger, "effect": effect,
+                                            "frequency": frequency})
+
+    def speed(self, speed: str):
+        self._speed = speed
+
+    def melee(self, desc: str, action: Actions = None, damage: str = None):
+        self._melees.append({"desc": desc, "action": action, "damage": damage})
+
+    def ranged(self, desc: str, action: Actions = None, damage: str = None):
+        self._ranged.append({"desc": desc, "action": action, "damage": damage})
+
+    def offensive_ability(self, name: str, action: Actions = None, trigger: str = None, effect: str = None,
+                          desc: str = None, frequency: str = None):
+        self._offensive_abilities.append({"name": name, "action": action, "trigger": trigger, "effect": effect,
+                                          "frequency": frequency})
