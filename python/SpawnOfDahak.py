c96a4dda2c020181692694a6b8d8fe1dd665c7df
Randy M. Roberts <rsqrdroberts@gmail.com>
2020-05-22 00:19:35 -0600

    o -- minor creature reference correction

--- python/SpawnOfDahak.py
+++ python/SpawnOfDahak.py
@@ -14,8 +14,8 @@ c.ac("27")
 c.saves("Fort +19", "Ref +13", "Will +16")
 c.hp("135")
 c.immunities("fire, paralyzed, sleep")
-c.reactive_ability("Tail Swipe", action=Actions.REACTION, trigger="A creature within reach of Racharak’s tail uses a move action or leaves a square during a move action it’s using.",
-                   effect="Racharak makes a tail Strike at the creature with a –2 penalty. If it hits, it disrupts the creature’s action.")
+c.reactive_ability("Tail Swipe", action=Actions.REACTION, trigger="A creature within reach of spawn of Dahak’s tail uses a move action or leaves a square during a move action it’s using.",
+                   effect="The spawn of Dahak makes a tail Strike at the creature with a –2 penalty. If it hits, it disrupts the creature’s action.")
 c.speed("30 feet, climb 30 feet")
 c.melee("warhammer +21 (magical, shove)", action=Actions.ONE,
         damage="2d8+9 bludgeoning (plus 1d5 fire if red-hot)")
@@ -26,12 +26,12 @@ c.melee("tail + 20 (reach 10 feet)",
 c.ranged("thrown rock + 17 (deadly 1d6, thrown 20 feet)",
          action=Actions.ONE, damage="2d6+9 bludgeoning")
 c.offensive_ability("Breath Weapon", action=Actions.TWO,
-                    desc="Racharak breathes flames that deals 9d6 fire damage to all creatures in a 30-foot cone(DC 26 basic Reflex save). It can’t use Breath Weapon again for 1d4 rounds.")
+                    desc="The spawn of Dahak breathes flames that deals 9d6 fire damage to all creatures in a 30-foot cone(DC 26 basic Reflex save). It can’t use Breath Weapon again for 1d4 rounds.")
 c.offensive_ability("Draconic Frenzy", action=Actions.TWO,
-                    desc="Racharak makes two claw Strikes and one bite Strike in any order.")
+                    desc="The spawn of Dahak makes two claw Strikes and one bite Strike in any order.")
 c.offensive_ability("Shrieking Frenzy", action=Actions.FREE,
-                    trigger="The charau-ka’s turn begins.", frequency="once per hour", effect="The charau-ka is quickened until the end of its turn, and can use the extra action to Stride or Strike. While in the frenzy, the charau-ka can’t speak and automatically critically fails Stealth checks, due to its loud wailing.")
-c.offensive_ability("Thrown Weapon Mastery", desc="Any weapon a charau-ka throws gains the deadly d6 weapon trait. Furthermore, when a charau-ka throws an improvised weapon, it does not take the –2 penalty for doing so, nor does it take a penalty for using a thrown improvised weapon with the nonlethal trait to make a lethal attack.")
+                    trigger="The spawn of Dahak’s turn begins.", frequency="once per hour", effect="The spawn of Dahak is quickened until the end of its turn, and can use the extra action to Stride or Strike. While in the frenzy, the spawn of Dahak can’t speak and automatically critically fails Stealth checks, due to its loud wailing.")
+c.offensive_ability("Thrown Weapon Mastery", desc="Any weapon a spawn of Dahak throws gains the deadly d6 weapon trait. Furthermore, when a spawn of Dahak throws an improvised weapon, it does not take the –2 penalty for doing so, nor does it take a penalty for using a thrown improvised weapon with the nonlethal trait to make a lethal attack.")
 # sg = c.spell_group("Extra Spells", dc=25, desc="attack +17")
 # spells = sg.spells(3)
 # spells.spell("fireball", id="119")
